b4c3df1aadb56f06660355aa7c3f9ba839134b13
mdmayfield <mdmayfield@users.noreply.github.com>
2017-04-16 22:00:36 -0500

    Add files via upload

--- Automation Envelope 0 In Time Selection.lua
+++ Automation Envelope 0 In Time Selection.lua
@@ -4,10 +4,10 @@ starttime, endtime = reaper.GetSet_LoopTimeRange2(0, false, false, 0, 0, false)
 if (envelope == nil) or (starttime == endtime) then goto ending
 end
 
-_,startvalue = reaper.Envelope_Evaluate(envelope, starttime, 0, 0)
-_,endvalue = reaper.Envelope_Evaluate(envelope, endtime, 0, 0)
+_,startvalue = reaper.Envelope_Evaluate(envelope, starttime-0.0000001, 0, 0)
+_,endvalue = reaper.Envelope_Evaluate(envelope, endtime+0.0000001, 0, 0)
 
-reaper.DeleteEnvelopePointRange(envelope, starttime, endtime+0.0001)
+reaper.DeleteEnvelopePointRange(envelope, starttime-0.0000001, endtime+0.0000001)
 
 reaper.InsertEnvelopePoint(envelope, starttime, startvalue, 0, 0, 0, true)
 reaper.InsertEnvelopePoint(envelope, starttime, 0, 0, 0, 0, true)
